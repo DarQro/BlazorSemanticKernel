0971d17e40e327fb2084ff5e44725fdcc70096dc
Chandler Crotchett <Chandler.Crotchett@sfbli.com>
2024-11-04 15:59:41 -0600

    asdf

--- BlazorSemanticKernel/obj/Debug/net8.0/BlazorSemanticKernel.AssemblyInfo.cs
+++ BlazorSemanticKernel/obj/Debug/net8.0/BlazorSemanticKernel.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("BlazorSemanticKernel")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f3a223a9ab7e7bbe057a3c9c2084e3842dbc4eec")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+192fb7a726edade1deb22a44c890c49cbd1a735e")]
 [assembly: System.Reflection.AssemblyProductAttribute("BlazorSemanticKernel")]
 [assembly: System.Reflection.AssemblyTitleAttribute("BlazorSemanticKernel")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
